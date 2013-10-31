0259787857da4b472f07a15a6d55979f2ad1f882
Judd Vinet <judd@betsmartmedia.com>
2013-10-31 13:09:28 -0700

    fixed postgres module to work with __ALL__

--- modules/postgresql.sh
+++ modules/postgresql.sh
@@ -3,12 +3,13 @@
 mod_postgresql() {
 	verbose "[POSTGRESQL] running..."
 	for db in "${POSTGRESQL_TARGETS[@]}"; do
+		cmd="pg_dump"
 		opts=""
 		[ "$POSTGRESQL_USER" != "" ] && opts="$opts -U ${POSTGRESQL_USER}"
 		[ "$POSTGRESQL_HOST" != "" ] && opts="$opts -h ${POSTGRESQL_HOST}"
 		[ "$POSTGRESQL_PORT" != "" ] && opts="$opts -p ${POSTGRESQL_PORT}"
 		if [ "$db" = "__ALL__" ]; then
-			opts="$opts -A"
+			cmd="pg_dumpall"
 			dbname="all"
 		else
 			opts="$opts $db"
@@ -18,11 +19,11 @@ mod_postgresql() {
 		verbose "[POSTGRESQL] backup up database: $db"
 		if [ "$POSTGRESQL_ENCRYPTION_KEY" ]; then
 			# decrypt with 'openssl enc -d -bf -pass pass:<password> -in infile -out outfile'
-			verbose "[POSTGRESQL] cmd: pg_dump $opts | bzip2 | openssl enc -e -salt -bf -pass pass:$POSTGRESQL_ENCRYPTION_KEY >$fn"
-			pg_dump $opts | bzip2 | openssl enc -e -salt -bf -pass pass:$POSTGRESQL_ENCRYPTION_KEY >$fn
+			verbose "[POSTGRESQL] cmd: $cmd $opts | bzip2 | openssl enc -e -salt -bf -pass pass:$POSTGRESQL_ENCRYPTION_KEY >$fn"
+			$cmd $opts | bzip2 | openssl enc -e -salt -bf -pass pass:$POSTGRESQL_ENCRYPTION_KEY >$fn
 		else
-			verbose "[POSTGRESQL] cmd: pg_dump $opts | bzip2 >$fn"
-			pg_dump $opts | bzip2 >$fn
+			verbose "[POSTGRESQL] cmd: $cmd $opts | bzip2 >$fn"
+			$cmd $opts | bzip2 >$fn
 		fi
 		[ $? -eq 0 ] || die "ERROR: command did not complete successfully"
 		NEW_BACKUPS="$NEW_BACKUPS $fn"
